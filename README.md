f11cebc0d2e5022e7d1c5ee6658abaaf71752d95
nosanity <no-sanity17@ya.ru>
2016-10-26 15:49:27 +0300

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+## Установка
+
+    Установить в plp:
+    pip install -e git+https://github.com/openprofession/plp-opro-payments@master#egg=opro_payments
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plp-opro-payments',
+    version='0.1',
+    packages=find_packages(),
+    description='PLP openprofession payments',
+    url='http://example.com',
+    author='author',
+)
