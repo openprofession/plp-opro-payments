1bc0eba80eb6c1669d7ca524751a104546e2dd1c
Nick Lubyanov <lubyanov@gmail.com>
2017-12-12 16:29:30 +0300

    OP-673 fix

--- opro_payments/views.py
+++ opro_payments/views.py
@@ -344,9 +344,9 @@ def gift_op_payment_view(request):
         try:
             gift_form = GiftForm(request.POST)
             if gift_form.is_valid():
-                if gift_form.cleaned_data['promocode']:
-                    result = apply_promocode(gift_form.cleaned_data['promocode'], module_id, 'edmodule' if module_id else 'course', session_id, only_first_course)
-                    new_price = result['new_price'] if result['status'] == 0 else None 
+                #if gift_form.cleaned_data['promocode']:
+                #    result = apply_promocode(gift_form.cleaned_data['promocode'], module_id, 'edmodule' if module_id else 'course', session_id, only_first_course)
+                #    new_price = result['new_price'] if result['status'] == 0 else None 
 
                 session, first_session_id, obj_price, total_price, products = get_obj_price(session_id, verified_enrollment, only_first_course, obj, upsales)
 
